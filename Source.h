6f4c5eb1d0f0cae7881706ef454c45e436f91e0d
jakee03 <jakefairham@rocketmail.com>
2026-03-14 13:33:50 +0000

    Source.h - first attempt at class creation

---
+++ Source.h
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+class RadioactiveSource {
+private:
+    std::string source_type;
+    std::string aquisition_date;
+    double activity; // in Becquerels
+    int source_id;
+
+public:
+    //constructors and destructor
+    RadioactiveSource();
+    RadioactiveSource(std::string type, std::string date, double act);
+    ~RadioactiveSource();
+
+    std::string getSourceType() const;
+    std::string getAquisitionDate() const;
+    double getActivity() const;
+    int getSourceID() const;
+
+    //setters
+    void setSourceType(const std::string type);
+    void setAquisitionDate(const std::string date);
+    void setActivity(double act);
+
+    //display
+    void printSourceData() const;
+};
