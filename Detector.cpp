8f537b5a262a5c1e87422c2466395095cdf8a8f9
jakee03 <jakefairham@rocketmail.com>
2026-03-14 14:15:38 +0000

    Detector.cpp - Initial skeleton code - CHANGE RANDOMISER LATER

---
+++ Detector.cpp
@@ -0,0 +1,17 @@
+#include "Detector.h"
+#include <iostream>
+#include <cstdlib> //for rand()
+
+RadiationDetector::RadiationDetector(std::string type)
+    : detector_type(type), is_on(false), total_counts(0) {}
+
+RadiationDetector::~RadiationDetector() {}
+
+int RadiationDetector::detectRadiation(const RadioactiveSource source) {
+    if (!is_on) return 0; // No counts if the detector is off
+
+    //simulation logic
+    int counts = static_cast<int>(source.getActivity() * (rand() % 10) / 100.0);
+    total_counts += counts;
+    return counts;
+}
